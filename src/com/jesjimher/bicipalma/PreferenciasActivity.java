15be472e7e79d736ccb51bf1bff82327fc54404a
jesjimher <jesjimenez@gmail.com>
2012-07-23 10:43:14 +0200

    Limpieza de imports

--- src/com/jesjimher/bicipalma/PreferenciasActivity.java
+++ src/com/jesjimher/bicipalma/PreferenciasActivity.java
@@ -1,11 +1,7 @@
 package com.jesjimher.bicipalma;
 
-import android.app.Activity;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.widget.Toast;
 
 public class PreferenciasActivity extends PreferenceActivity {
 	@Override
