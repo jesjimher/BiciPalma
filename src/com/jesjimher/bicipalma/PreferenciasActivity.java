cd07f7b46a3e711c8e603f3f7dff1aa8fb222159
jesjimher <jesjimenez@gmail.com>
2012-07-27 09:22:31 +0200

    TODO

--- src/com/jesjimher/bicipalma/PreferenciasActivity.java
+++ src/com/jesjimher/bicipalma/PreferenciasActivity.java
@@ -2,7 +2,7 @@ package com.jesjimher.bicipalma;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-
+//TODO: Forzar idioma para el que lo quiera en cat pero no tenga android en cat
 public class PreferenciasActivity extends PreferenceActivity {
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
